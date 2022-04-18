7b7a6a5d8ccfee66e26b3823442c76ce6faa4f6d
bigben35 <79405849+bigben35@users.noreply.github.com>
2022-04-18 23:07:45 +0200

    correction faille xss dans search.php

--- app/Views/front/template/search.php
+++ app/Views/front/template/search.php
@@ -1,6 +1,6 @@
 <form method="GET" action="blog">
                 <div>
-                    <input type="search" name="query" placeholder="Rechercher un article ..." value="<?php if(isset($_GET['query'])) echo $_GET['query'] ?>">
+                    <input type="search" name="query" placeholder="Rechercher un article ..." value="<?php if(isset($_GET['query'])) echo $query ?>">
                 </div>
                 <div>
                     <input type="submit" value="Go!">
